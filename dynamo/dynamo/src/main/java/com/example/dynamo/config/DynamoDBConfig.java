d9f7e49b0b95ab7929b44191d2b66249708e370d
TaisAndrade <taismdeandrade@gmail.com>
2025-05-08 14:11:24 -0300

    fix: adiciona as credenciais

--- dynamo/dynamo/src/main/java/com/example/dynamo/config/DynamoDBConfig.java
+++ dynamo/dynamo/src/main/java/com/example/dynamo/config/DynamoDBConfig.java
@@ -2,6 +2,8 @@ package com.example.dynamo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 
@@ -9,9 +11,15 @@ import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 public class DynamoDBConfig {
 
     @Bean
-    public DynamoDbClient dynamoDbClient(){
+    public AwsCredentialsProvider awsCredentialsProvider() {
+        return DefaultCredentialsProvider.create();
+    }
+
+    @Bean
+    public DynamoDbClient dynamoDbClient(AwsCredentialsProvider credentialsProvider) {
         return DynamoDbClient.builder()
                 .region(Region.US_EAST_1)
+                .credentialsProvider(credentialsProvider)
                 .build();
     }
 }
