ffe3adbac43c676e1dcf324ad4aad496644db033
TaisAndrade <taismdeandrade@gmail.com>
2025-05-08 09:01:19 -0300

    feat: cria o ItemEntity

---
+++ dynamo/dynamo/src/main/java/com/example/dynamo/entities/ItemEntity.java
@@ -0,0 +1,60 @@
+package com.example.dynamo.entities;
+
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbAttribute;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbBean;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbPartitionKey;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbSortKey;
+
+import java.util.Date;
+import java.util.UUID;
+
+@DynamoDbBean
+public class ItemEntity {
+
+    private UUID itemId;
+    private Date data;
+    private String nome;
+    private String status;
+
+    public ItemEntity() {
+
+    }
+
+    @DynamoDbSortKey
+    @DynamoDbAttribute("item_id")
+    public UUID getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(UUID itemId) {
+        this.itemId = itemId;
+    }
+
+    @DynamoDbPartitionKey
+    @DynamoDbAttribute("data")
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+
+    @DynamoDbAttribute("nome")
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @DynamoDbAttribute("status")
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
