d96ec4e81697ad9dbcd2bd3f518c5bbd602e225a
TaisAndrade <taismdeandrade@gmail.com>
2025-05-14 15:19:45 -0300

    fix: corrige o main adicionando o remove no terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -24,7 +24,32 @@ module "add_item_lambda" {
     DYNAMODB_TABLE_NAME = aws_dynamodb_table.item_table.name
   }
 }
+module "edit_item_lambda" {
+  source = "./modules/lambda"
 
+  function_name = "edit_item"
+  runtime       = "java21"
+  handler       = "com.example.EditItemHandler"
+  filename      = "../lambda/edit-item/target/edit-item-1.0-SNAPSHOT.jar"
+  role_arn      = aws_iam_role.lambda_dynamodb_role.arn
+  environment_variables = {
+    DYNAMODB_TABLE_NAME = aws_dynamodb_table.item_table.name
+  }
+}
+
+
+module "remove_item_lambda" {
+  source = "./modules/lambda"
+
+  function_name = "remove_item"
+  runtime       = "java21"
+  handler       = "com.example.DeleteItemHandler"
+  filename      = "../lambda/remove-item/target/remove-item-1.0-SNAPSHOT.jar"
+  role_arn      = aws_iam_role.lambda_dynamodb_role.arn
+  environment_variables = {
+    DYNAMODB_TABLE_NAME = aws_dynamodb_table.item_table.name
+  }
+}
 resource "aws_dynamodb_table" "item_table" {
   name           = "item"
   billing_mode   = "PAY_PER_REQUEST"
