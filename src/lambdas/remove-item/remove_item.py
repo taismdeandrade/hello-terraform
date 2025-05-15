5f0019b867ca4f087b0a9d763e29ba6f78ecfbe7
TaisAndrade <taismdeandrade@gmail.com>
2025-05-15 16:38:34 -0300

    feat: adiciona a lambda remove_item

---
+++ src/lambdas/remove-item/remove_item.py
@@ -0,0 +1,27 @@
+import json
+import os
+import boto3
+import uuid
+
+dynamodb = boto3.resource('dynamodb')
+nome_tabela = os.environ['NOME_TABELA']
+tabela = dynamodb.Table(nome_tabela)
+
+def remove_item_handler(event, context):
+    try:
+        tabela.delete_item(Key={
+            'SK': event['sk'],
+            'PK': event['pk'],
+               
+        })
+        return {
+            'mensagem': 'Item removido com sucesso',   
+        }
+    except Exception as e:
+        return {
+            'statusCode': 500,
+            'body': json.dumps({
+                'message': 'Erro ao remover o item',
+                'error': str(e)
+            })
+        }
