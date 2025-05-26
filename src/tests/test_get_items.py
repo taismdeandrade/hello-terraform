dae9cccb280e71f945fffcb07072a63c9700ba16
LauriESB <ich.lauri@gmail.com>
2025-05-26 00:52:16 -0300

    chore: format test files using Black

--- src/tests/test_get_items.py
+++ src/tests/test_get_items.py
@@ -1,24 +1,26 @@
 import json
 from lambdas.get_items.get_items import get_items_handler
 
+
 def test_get_items_success(mock_dynamodb_table, sample_item):
 
     expected_items = [sample_item]
     mock_dynamodb_table.scan.return_value = {"Items": expected_items}
 
-    response = get_items_handler({},{})
+    response = get_items_handler({}, {})
 
     assert response["statusCode"] == 200
     assert json.loads(response["body"]) == expected_items
 
+
 def test_get_items_empty_table(mock_dynamodb_table):
 
     mock_dynamodb_table.scan.return_value = {"Items": []}
-    
+
     response = get_items_handler({}, {})
-    
-    assert response["statusCode"] == 200  
-    assert json.loads(response["body"]) == [] 
+
+    assert response["statusCode"] == 200
+    assert json.loads(response["body"]) == []
 
 
 def test_get_items_failure(mock_dynamodb_table):
@@ -29,5 +31,3 @@ def test_get_items_failure(mock_dynamodb_table):
 
     assert response["statusCode"] == 500
     assert "simulated error" in json.loads(response["body"])["error"]
-
-    
